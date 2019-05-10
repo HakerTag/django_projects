6fccb234ec0994cafb65b5534cd04920a4d9b573
hakertag <hakertaggroup@gmail.com>
2019-05-11 04:58:56 +0600

    Final project crud by siraj using postgresql

---
+++ CRUD/Crudapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post  # add this to import the Post model
+
+# Register your models here.
+
+admin.site.register(Post)  # add this to register the Post model
